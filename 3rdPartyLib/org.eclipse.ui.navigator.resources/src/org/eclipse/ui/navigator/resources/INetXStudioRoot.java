bd5f0bc88e19678c09f7c61b6fb63915ee746d0d
Christophe Bouhier <dzonekl@gmail.com>
2011-05-08 20:28:00 +0000

    package rename to original package name. [We have a custom navigator]

---
+++ 3rdPartyLib/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/navigator/resources/INetXStudioRoot.java
@@ -0,0 +1,18 @@
+package org.eclipse.ui.navigator.resources;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.emf.common.util.URI;
+
+public interface INetXStudioRoot extends  IAdaptable {
+	
+	/**
+	 * Return the DB as a URI. 
+	 * @return
+	 */
+	public URI[] getDatabases();
+
+	
+ 	public IProject[] getProjects();
+
+}
