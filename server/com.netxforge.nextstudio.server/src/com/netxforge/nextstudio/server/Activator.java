319ca4ebe253f539ae760b3e5d1642b8a56f0313
Martin Taal <mtaal@elver.org>
2011-05-20 11:25:40 +0000

    Updated copyright statement

--- server/com.netxforge.nextstudio.server/src/com/netxforge/nextstudio/server/Activator.java
+++ server/com.netxforge.nextstudio.server/src/com/netxforge/nextstudio/server/Activator.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) May 20, 2011 NetXForge.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  You should have received a copy of the GNU Lesser General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * 
+ * Contributors: 
+ * 	Martin Taal - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 package com.netxforge.nextstudio.server;
 
 import org.osgi.framework.BundleActivator;
@@ -15,6 +33,7 @@ public class Activator implements BundleActivator {
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
 	}
@@ -23,6 +42,7 @@ public class Activator implements BundleActivator {
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
 	}
