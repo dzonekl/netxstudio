af56e68b9cc6ce5fbf20836377d6bf0cbd38647f
Martin Taal <mtaal@elver.org>
2011-06-20 15:47:55 +0000

    Changed way JobImplementation is instantiated

--- server/com.netxforge.netxstudio.server.metrics/src/com/netxforge/netxstudio/server/metrics/Activator.java
+++ server/com.netxforge.netxstudio.server.metrics/src/com/netxforge/netxstudio/server/metrics/Activator.java
@@ -3,8 +3,11 @@ package com.netxforge.netxstudio.server.metrics;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.util.Modules;
 import com.netxforge.netxstudio.scheduling.MetricSourceJob;
 import com.netxforge.netxstudio.server.ServerModule;
 import com.netxforge.netxstudio.server.job.JobImplementation;
@@ -14,7 +17,7 @@ public class Activator implements BundleActivator {
 
 	private static BundleContext context;
 	private static Activator INSTANCE;
-	
+
 	static BundleContext getContext() {
 		return context;
 	}
@@ -22,34 +25,41 @@ public class Activator implements BundleActivator {
 	public static Activator getInstance() {
 		return INSTANCE;
 	}
-	
+
 	private Injector injector;
-	
+
 	/*
 	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	@Override
 	public void start(BundleContext bundleContext) throws Exception {
 		INSTANCE = this;
 		Activator.context = bundleContext;
 
-		injector = Guice.createInjector(ServerModule.getModule());
-		
+		Module om = new MetricModule();
+		om = Modules.override(om).with(ServerModule.getModule());
+		injector = Guice.createInjector(om);
+
 		// register our new MetricSourceJob creator
-		JobImplementation.REGISTRY.register(MetricSourceJob.class, new JobImplementationFactory() {
-			@Override
-			public JobImplementation create() {
-				final MetricSourceJobImplementation jobImplementation = new MetricSourceJobImplementation();
-				injector.injectMembers(jobImplementation);
-				return jobImplementation;
-			}
-		});
+		JobImplementation.REGISTRY.register(MetricSourceJob.class,
+				new JobImplementationFactory() {
+					@Override
+					public JobImplementation create() {
+						return injector
+								.getInstance(MetricSourceJobImplementation.class);
+					}
+				});
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
@@ -60,4 +70,12 @@ public class Activator implements BundleActivator {
 		return injector;
 	}
 
+	public class MetricModule extends AbstractModule {
+
+		// Bind our expression engine.
+		@Override
+		protected void configure() {
+			this.bind(MetricSourceJobImplementation.class);
+		}
+	}
 }
