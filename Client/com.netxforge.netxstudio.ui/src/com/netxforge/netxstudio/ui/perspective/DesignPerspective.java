b5a6f99505ccfc58754091210a63503be0bb1c43
Christophe Bouhier <dzonekl@gmail.com>
2011-07-28 12:20:44 +0000

    UI Iteration #3

--- Client/com.netxforge.netxstudio.ui/src/com/netxforge/netxstudio/ui/perspective/DesignPerspective.java
+++ Client/com.netxforge.netxstudio.ui/src/com/netxforge/netxstudio/ui/perspective/DesignPerspective.java
@@ -1,21 +1,15 @@
 package com.netxforge.netxstudio.ui.perspective;
 
-import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
-import org.eclipse.ui.IPerspectiveFactory;
 
-public class DesignPerspective implements IPerspectiveFactory {
+public class DesignPerspective extends AbstractFormPerspective {
 	
 	public static String ID = "com.netxforge.netxstudio.ui.perspective.design";
 	
 	public void createInitialLayout(IPageLayout layout) {
-		layout.setEditorAreaVisible(true);
-		String editorArea = layout.getEditorArea();
-		IFolderLayout left = layout.createFolder(
-				"left", IPageLayout.LEFT, 0.3f,editorArea); //$NON-NLS-1$	 //$NON-NLS-2$
-		left.addView("com.netxforge.netxstudio.navigator.ProjectExplorer");
-		left.addView(IPageLayout.ID_PROP_SHEET);
 		
+		super.createInitialLayout(layout);
+		layout.addStandaloneView("com.netxforge.netxstudio.screens.selector.design", false, IPageLayout.LEFT, 0.5f, IPageLayout.ID_EDITOR_AREA);		
 		layout.addActionSet("com.netxforge.netxstudio.ui.designActionSet");
 		
 	}
