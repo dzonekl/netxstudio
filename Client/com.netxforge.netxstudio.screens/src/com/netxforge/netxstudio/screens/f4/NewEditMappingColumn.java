60dcf070b636e495b1eb471ed2ef810416327d02
Christophe Bouhier <dzonekl@gmail.com>
2011-09-25 08:49:09 +0000

    --gitcommit

--- Client/com.netxforge.netxstudio.screens/src/com/netxforge/netxstudio/screens/f4/NewEditMappingColumn.java
+++ Client/com.netxforge.netxstudio.screens/src/com/netxforge/netxstudio/screens/f4/NewEditMappingColumn.java
@@ -25,16 +25,19 @@ import org.eclipse.emf.edit.command.AddCommand;
 import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.jface.databinding.swt.ISWTObservableValue;
 import org.eclipse.jface.databinding.swt.SWTObservables;
+import org.eclipse.jface.databinding.viewers.IViewerObservableValue;
+import org.eclipse.jface.databinding.viewers.ViewerProperties;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ComboViewer;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -46,6 +49,7 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.events.HyperlinkEvent;
 import org.eclipse.ui.forms.events.IHyperlinkListener;
+import org.eclipse.ui.forms.widgets.ColumnLayout;
 import org.eclipse.ui.forms.widgets.Form;
 import org.eclipse.ui.forms.widgets.FormText;
 import org.eclipse.ui.forms.widgets.FormToolkit;
@@ -57,6 +61,7 @@ import com.google.common.collect.Lists;
 import com.netxforge.netxstudio.common.model.ModelUtils;
 import com.netxforge.netxstudio.metrics.DataKind;
 import com.netxforge.netxstudio.metrics.IdentifierDataKind;
+import com.netxforge.netxstudio.metrics.KindHintType;
 import com.netxforge.netxstudio.metrics.MappingColumn;
 import com.netxforge.netxstudio.metrics.Metric;
 import com.netxforge.netxstudio.metrics.MetricsFactory;
@@ -114,6 +119,9 @@ public class NewEditMappingColumn extends AbstractScreen implements
 	private ISWTObservableValue identifierPatternObservable;
 	private ISWTObservableValue metricObservable;
 	private ISWTObservableValue valuePatternObservable;
+	private Combo combo;
+	private ComboViewer comboViewerMetricKindHint;
+	private IViewerObservableValue metricKindHintObservable;
 
 	/**
 	 * Create the composite.
@@ -130,15 +138,11 @@ public class NewEditMappingColumn extends AbstractScreen implements
 		});
 		toolkit.adapt(this);
 		toolkit.paintBordersFor(this);
-<<<<<<< .mine
 
 		// Remove when runtime,.
 		// buildUI();
 		// buildHeaderMappingOptions(cmpColumnMapping);
 		// buildDataMappingOptions(cmpColumnMapping);
-=======
-		// buildUI();
->>>>>>> .r761
 	}
 
 	private void buildUI() {
@@ -154,16 +158,16 @@ public class NewEditMappingColumn extends AbstractScreen implements
 
 		frmNewMappingColumn.setText(actionText + "Mapping Column");
 
-		frmNewMappingColumn.getBody().setLayout(new FormLayout());
+		frmNewMappingColumn.getBody().setLayout(new ColumnLayout());
 
 		Section sctnMappings = toolkit.createSection(
 				frmNewMappingColumn.getBody(), Section.EXPANDED
 						| Section.TITLE_BAR);
-		FormData fd_sctnMappings = new FormData();
-		fd_sctnMappings.top = new FormAttachment(0, 10);
-		fd_sctnMappings.right = new FormAttachment(100, -12);
-		fd_sctnMappings.left = new FormAttachment(0, 12);
-		sctnMappings.setLayoutData(fd_sctnMappings);
+		// FormData fd_sctnMappings = new FormData();
+		// fd_sctnMappings.top = new FormAttachment(0, 10);
+		// fd_sctnMappings.right = new FormAttachment(100, -12);
+		// fd_sctnMappings.left = new FormAttachment(0, 12);
+		// sctnMappings.setLayoutData(fd_sctnMappings);
 		toolkit.paintBordersFor(sctnMappings);
 		sctnMappings.setText("Info");
 
@@ -186,13 +190,13 @@ public class NewEditMappingColumn extends AbstractScreen implements
 		Section sctnDataKind = toolkit.createSection(
 				frmNewMappingColumn.getBody(), Section.EXPANDED
 						| Section.TITLE_BAR);
-		fd_sctnMappings.bottom = new FormAttachment(sctnDataKind, -6);
-		FormData fd_sctnNewSection = new FormData();
-		fd_sctnNewSection.bottom = new FormAttachment(100, -12);
-		fd_sctnNewSection.top = new FormAttachment(0, 88);
-		fd_sctnNewSection.left = new FormAttachment(0, 12);
-		fd_sctnNewSection.right = new FormAttachment(100, -12);
-		sctnDataKind.setLayoutData(fd_sctnNewSection);
+		// fd_sctnMappings.bottom = new FormAttachment(sctnDataKind, -6);
+		// FormData fd_sctnNewSection = new FormData();
+		// fd_sctnNewSection.bottom = new FormAttachment(100, -12);
+		// fd_sctnNewSection.top = new FormAttachment(0, 88);
+		// fd_sctnNewSection.left = new FormAttachment(0, 12);
+		// fd_sctnNewSection.right = new FormAttachment(100, -12);
+		// sctnDataKind.setLayoutData(fd_sctnNewSection);
 		toolkit.paintBordersFor(sctnDataKind);
 		sctnDataKind.setText("Mapping");
 		sctnDataKind.setExpanded(true);
@@ -268,7 +272,12 @@ public class NewEditMappingColumn extends AbstractScreen implements
 		txtMetric.setText("");
 		txtMetric.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
 				false, 1, 1));
-		new Label(cmpColumnMapping, SWT.NONE);
+
+		comboViewerMetricKindHint = new ComboViewer(cmpColumnMapping, SWT.NONE);
+		combo = comboViewerMetricKindHint.getCombo();
+		combo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1,
+				1));
+		toolkit.paintBordersFor(combo);
 
 	}
 
@@ -486,10 +495,9 @@ public class NewEditMappingColumn extends AbstractScreen implements
 				editingService.getEditingDomain(),
 				MetricsPackage.Literals.MAPPING_COLUMN__DATA_TYPE);
 
-		initDataBindingHeaderMappingColumn(context, aggregate);
+		initDataBindingHeaderMappingColumn(context);
 		if (showDataMapping) {
-			initDataBindingDataMappingColumn(context, aggregate,
-					dataKindProperty);
+			initDataBindingDataMappingColumn(context, dataKindProperty);
 		}
 
 		// Should occure before the aggregator is enabled.
@@ -512,7 +520,7 @@ public class NewEditMappingColumn extends AbstractScreen implements
 	 * bind the part of the UI which deals with header mapping.
 	 */
 	private void initDataBindingHeaderMappingColumn(
-			EMFDataBindingContext context, DatakindAggregate aggregate) {
+			EMFDataBindingContext context) {
 
 		// ///////////////////////////
 		// WRITABLE OBSERVABLES MAPPING COLUMN KIND
@@ -604,14 +612,14 @@ public class NewEditMappingColumn extends AbstractScreen implements
 		
 		// Value observables. 
 		valuePatternObservable.addValueChangeListener(aggregate);
+		metricKindHintObservable.addValueChangeListener(aggregate);
 	}
 
 	/*
 	 * bind the part of the UI which deals with data mapping.
 	 */
 	private void initDataBindingDataMappingColumn(
-			EMFDataBindingContext context, DatakindAggregate aggregate,
-			IEMFValueProperty dataKindProperty) {
+			EMFDataBindingContext context, IEMFValueProperty dataKindProperty) {
 
 		btnMetricWritableValue = new WritableValue();
 
@@ -637,11 +645,12 @@ public class NewEditMappingColumn extends AbstractScreen implements
 						return null;
 					}
 				});
+
 		context.bindValue(metricValueObservable,
 				dataKindProperty.observe(mxlsColumn), null,
 				metricModelToTargetStrategy);
+
 		context.bindValue(metricObservable, btnMetricWritableValue, null, null);
-<<<<<<< .mine
 
 		comboViewerMetricKindHint
 				.setContentProvider(new ArrayContentProvider());
@@ -651,8 +660,6 @@ public class NewEditMappingColumn extends AbstractScreen implements
 		metricKindHintObservable = ViewerProperties.singleSelection().observe(
 				comboViewerMetricKindHint);
 
-=======
->>>>>>> .r761
 	}
 
 	abstract class DataKindModelToTargetConverter implements IConverter {
@@ -706,21 +713,12 @@ public class NewEditMappingColumn extends AbstractScreen implements
 					cmbTimePattern.setText(vdk.getFormat());
 				}
 			}
-<<<<<<< .mine
 			// if (vkt == ValueKindType.METRIC) {
 			// this.btnMetricWritableValue.setValue(true);
 			// if (vdk.getFormat() != null) {
 			// this.txtMetricValuePattern.setText(vdk.getFormat());
 			// }
 			// }
-=======
-			if (vkt == ValueKindType.METRIC) {
-				this.btnMetricWritableValue.setValue(true);
-				if (vdk.getFormat() != null) {
-					this.txtMetricValuePattern.setText(vdk.getFormat());
-				}
-			}
->>>>>>> .r761
 		}
 		if (value instanceof IdentifierDataKind) {
 			this.btnIdentifierWritableValue.setValue(true);
@@ -741,14 +739,11 @@ public class NewEditMappingColumn extends AbstractScreen implements
 				if (vdk.getFormat() != null) {
 					this.txtMetricValuePattern.setText(vdk.getFormat());
 				}
-<<<<<<< .mine
 				if (vdk.eIsSet(MetricsPackage.Literals.VALUE_DATA_KIND__KIND_HINT)) {
 					KindHintType kindHint = vdk.getKindHint();
 					comboViewerMetricKindHint
 							.setSelection(new StructuredSelection(kindHint));
 				}
-=======
->>>>>>> .r761
 			}
 		}
 	}
@@ -758,12 +753,8 @@ public class NewEditMappingColumn extends AbstractScreen implements
 		private IObservableValue dataKindObservable;
 
 		private String pattern; // stores the pattern for the datakind.
-<<<<<<< .mine
 		private int metricKindHint = -1;
 
-=======
-
->>>>>>> .r761
 		boolean datetime = false;
 		boolean date = false;
 		boolean time = false;
@@ -828,7 +819,6 @@ public class NewEditMappingColumn extends AbstractScreen implements
 					modelUpdate();
 				}
 			}
-<<<<<<< .mine
 
 			if (event.getObservable() instanceof IViewerObservableValue) {
 				if (newValue instanceof KindHintType) {
@@ -837,8 +827,6 @@ public class NewEditMappingColumn extends AbstractScreen implements
 				}
 			}
 
-=======
->>>>>>> .r761
 		}
 
 		private boolean modelUpdate() {
@@ -922,6 +910,7 @@ public class NewEditMappingColumn extends AbstractScreen implements
 						MetricsPackage.Literals.IDENTIFIER_DATA_KIND__PATTERN,
 						pattern);
 				cc.append(sc);
+				// ((IdentifierDataKind) dk).setPattern(pattern);
 			}
 			editingService.getEditingDomain().getCommandStack().execute(cc);
 			dataKindObservable.setValue(dk);
