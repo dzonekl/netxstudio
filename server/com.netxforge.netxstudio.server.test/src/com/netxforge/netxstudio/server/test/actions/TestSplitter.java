a7a42337fc8bb4305720e9339655e0cfc6f880d1
Christophe Bouhier <dzonekl@gmail.com>
2011-11-03 13:20:24 +0000

    matching tests.

--- server/com.netxforge.netxstudio.server.test/src/com/netxforge/netxstudio/server/test/actions/TestSplitter.java
+++ server/com.netxforge.netxstudio.server.test/src/com/netxforge/netxstudio/server/test/actions/TestSplitter.java
@@ -18,40 +18,141 @@
  *******************************************************************************/
 package com.netxforge.netxstudio.server.test.actions;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.junit.Test;
 
-
 /**
- * Test google collect splitter.
- * @author Christophe Bouhier christophe.bouhier@netxforge.com 
+ * 
+ * http://mindprod.com/jgloss/regex.html
+ * 
+ * @author Christophe Bouhier christophe.bouhier@netxforge.com
  */
 public class TestSplitter {
 
-	@Test
-	public void testSplitter_1() {
-		this.doTestFor("A,B,C", ",");
-		this.doTestFor("A,B,C", "\"");
-	}
+	String testString = "Result Time,Granularity Period,Object Name,Reliability,\"Absolutely Average CPU occupation ratio\",\"Absolutely maximum CPU occupation ratio\","
+			+ "\"Process CPU congestion duration\",\"Process CPU overload duration\"";
+	String testString2 = "2011-10-18 12:00,60,\"RTSGSN1/Process:Process No.=0, Process type=LIP, Slot No.=8, " +
+			"Subrack No.=1\",Reliable,12,16,0,0a";
+	String testString3 = "\",A,B,C\",\"E\",D";
+
+	String testString4 = "RTSGSN1/Process:Process No.=0, Process type=LIP, Slot No.=8, Subrack No.=1";
+	
+	String QUOTED_OR_NOT = "(\".*?\"|.*?)";
+
+	Pattern csvMatcher = Pattern.compile( "(\".*?\"|.*?)," , Pattern.MULTILINE | Pattern.DOTALL);
+
+	Pattern csvSplitter = Pattern.compile( "[,(\".*?\")]++");
+	
+	
+	
 	
 	@Test
-	public void testSplitter_2() {
-		this.doTestFor("\"A,B,C\",D,E", "\"");
-	}
-	
-	@Test
-	public void testSplitter_3() {
-		String[]  result = this.doTestFor("\"A,B,C\",D,E", "\"");
-		for(int j = 0; j < result.length ; j++){
-			this.doTestFor(result[j], ",");
+	public void testCSVQuoted() {
+		System.out.println("-------");
+		testString = testString.trim();
+		Matcher matcher = csvMatcher.matcher(testString);
+		while (matcher.find()) {
+			final int gc = matcher.groupCount();
+			// group 0 is the whole pattern matched,
+			// loops runs from from 0 to gc, not 0 to gc-1 as is traditional.
+			for (int i = 0; i <= gc; i++) {
+				System.out.println(i + " : " + matcher.group(i));
+			}
 		}
+
 	}
 	
-	String[] doTestFor(String s, String pattern){
-		String[] split = s.split(pattern);
-		for(int i = 0; i < split.length ; i++){
-			System.out.println(split[i]);
+	
+	
+	
+	@Test
+	public void testCSVQuoted2() {
+
+		System.out.println("-------");
+		Matcher matcher = csvMatcher.matcher(testString2);
+		while (matcher.find()) {
+			final int gc = matcher.groupCount();
+			// group 0 is the whole pattern matched,
+			// loops runs from from 0 to gc, not 0 to gc-1 as is traditional.
+			for (int i = 0; i <= gc; i++) {
+				System.out.println(i + " : " + matcher.group(i));
+			}
 		}
-		return split;
 	}
 
+	@Test
+	public void testCSVSplitter() {
+		System.out.println("-------");
+		
+		String[] split = csvSplitter.split(testString2);
+		for(int i = 0; i < split.length ; i++){
+			System.out.println(i + ": "+ split[i]);
+		}
+	}
+	
+	/*
+	 * Extracts RTSGSN1 in: 
+	 * RTSGSN1/Process:Process No.=0, Process type=LIP, Slot No.=8, Subrack No.=1
+	 */
+	Pattern nodeIDExtractor = Pattern.compile("^(\\w+)");
+	
+	@Test
+	public void testValueExtractor_NodeID() {
+		System.out.println("------- Node ID Extractor:");
+		
+		int index = -1;
+		Matcher matcher = nodeIDExtractor.matcher(testString4);
+		if(matcher.find()) {
+			index = matcher.end();
+		}
+
+		if(index != -1){
+			System.out.println(testString4.substring(0,index));
+		}
+	}
+	
+	/*
+	 * Extracts LIP in: 
+	 * RTSGSN1/Process:Process No.=0, Process type=LIP, Slot No.=8, Subrack No.=1
+	 */
+	Pattern procesTypeExtractor = Pattern.compile("Process type=(LIP|LLP|SGP|GBP)");
+	
+	@Test
+	public void testValueExtractor_ProcessType() {
+		System.out.println("------- Process Type Extractor: ");
+		
+		Matcher matcher = procesTypeExtractor.matcher(testString4);
+		if(matcher.find()) {
+			int gc = matcher.groupCount();
+			for (int i = 0; i <= gc; i++) {
+				// Skip group 0 as usual. 
+				System.out.println(i + " : " + matcher.group(i));
+			}
+		}
+	}
+	
+	
+	/*
+	 * Extracts LIP in: 
+	 * RTSGSN1/Process:Process No.=0, Process type=LIP, Slot No.=8, Subrack No.=1
+	 */
+	Pattern SlotNoExtractor = Pattern.compile("Slot No\\.=(\\d++)");
+	
+	@Test
+	public void testValueExtractor_Slot() {
+		System.out.println("------- Process Slot Extractor: ");
+		
+		Matcher matcher = SlotNoExtractor.matcher(testString4);
+		if(matcher.find()) {
+			int gc = matcher.groupCount();
+			for (int i = 0; i <= gc; i++) {
+				// Skip group 0 as usual. 
+				System.out.println(i + " : " + matcher.group(i));
+			}
+		}
+	}
+	
+	
 }
