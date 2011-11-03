c34838650fd4798fafda2f74f1c6ddbeba9238a5
Christophe Bouhier <dzonekl@gmail.com>
2011-11-03 13:13:06 +0000

    - bug 159.

--- Client/com.netxforge.netxstudio.data/src/com/netxforge/netxstudio/data/importer/CSVMetricValuesImporter.java
+++ Client/com.netxforge.netxstudio.data/src/com/netxforge/netxstudio/data/importer/CSVMetricValuesImporter.java
@@ -23,9 +23,12 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import com.google.common.collect.Lists;
 import com.netxforge.netxstudio.metrics.MappingCSV;
+import com.netxforge.netxstudio.metrics.MetricsPackage;
 
 /**
  * Imports metrics from CVS.
@@ -35,6 +38,7 @@ import com.netxforge.netxstudio.metrics.MappingCSV;
 public class CSVMetricValuesImporter extends AbstractMetricValuesImporter {
 
 	private String[][] data = null;
+	private static final String DEFAULT_DELIMITER = ",";
 	
 	@Override
 	protected int processFile(File file) throws Exception {
@@ -58,24 +62,80 @@ public class CSVMetricValuesImporter extends AbstractMetricValuesImporter {
 	}
 
 	private void setData(FileReader fileReader) throws Exception {
-		final BufferedReader reader = new BufferedReader(fileReader);
-		String delimiter = ((MappingCSV)getMapping()).getDelimiter();
 		
-		if(delimiter == null){
-			delimiter = ",";
+		final BufferedReader reader = new BufferedReader(fileReader);
+		
+		if(!(getMapping() instanceof MappingCSV)){
+			return;
 		}
 		
+		MappingCSV mapping = (MappingCSV) this.getMapping();
+		final String delimiter = mapping != null
+				&& mapping
+						.eIsSet(MetricsPackage.Literals.MAPPING_CSV__DELIMITER) ? mapping
+				.getDelimiter() : DEFAULT_DELIMITER;
+
+		// Pattern p = Pattern.compile("^" + QUOTED_OR_NOT + "?" + delimiter
+		// + QUOTED_OR_NOT + delimiter + QUOTED_OR_NOT + "?");
+
+		Pattern p = Pattern.compile("(\".*?\"|.*?)" + delimiter);
+
 		final List<String[]> localData = new ArrayList<String[]>();
 		String line;
 		while ((line = reader.readLine()) != null) {
-			final StringTokenizer tokenizer = new StringTokenizer(line, delimiter);
-			final List<String> lineData = new ArrayList<String>();
-			while (tokenizer.hasMoreElements()) {
-				lineData.add(convert(tokenizer.nextToken()));
+
+			// Split (by default) for encapsulated by quote char...
+			// CB: http://work.netxforge.com/issues/159
+			// See also: http://mindprod.com/jgloss/regex.html
+			if (line.isEmpty())
+				continue;
+			ArrayList<String> matchingList = Lists.newArrayList();
+			Matcher matcher = p.matcher(line);
+			while (matcher.find()) {
+				final int gc = matcher.groupCount();
+				// group 0 is the whole pattern matched,
+				// loops runs from from 0 to gc, not 0 to gc-1 as is
+				// traditional.
+				for (int i = 0; i <= gc; i++) {
+					String match = matcher.group(i);
+					System.out.println(i + " : " + match);
+					if (i > 0) { // Skip the outer group.
+						matchingList.add(match);
+					}
+				}
 			}
+
+			final List<String> lineData = new ArrayList<String>();
+
+			for (String s : matchingList) {
+				lineData.add(convert(s));
+			}
+
 			localData.add(lineData.toArray(new String[lineData.size()]));
 		}
 		data = localData.toArray(new String[localData.size()][]);
+		
+//		String delimiter = ((MappingCSV)getMapping()).getDelimiter();
+//		
+//		if(delimiter == null){
+//			delimiter = ",";
+//		}
+//		
+//		final List<String[]> localData = new ArrayList<String[]>();
+//		String line;
+//		while ((line = reader.readLine()) != null) {
+//			
+//			// FIXME, Double tokenizer...
+//			
+//			
+//			final StringTokenizer tokenizer = new StringTokenizer(line, delimiter);
+//			final List<String> lineData = new ArrayList<String>();
+//			while (tokenizer.hasMoreElements()) {
+//				lineData.add(convert(tokenizer.nextToken()));
+//			}
+//			localData.add(lineData.toArray(new String[lineData.size()]));
+//		}
+//		data = localData.toArray(new String[localData.size()][]);
 	}
 	
 	private String convert(String value) {
