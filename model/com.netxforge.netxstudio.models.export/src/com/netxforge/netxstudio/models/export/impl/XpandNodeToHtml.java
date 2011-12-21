12e996bd6008d48d4bac5f7071b3bbc2075750fb
Christophe Bouhier <dzonekl@gmail.com>
2011-12-21 10:23:58 +0000

    - nodeToHtml was never tested, it had a few issues.

--- model/com.netxforge.netxstudio.models.export/src/com/netxforge/netxstudio/models/export/impl/XpandNodeToHtml.java
+++ model/com.netxforge.netxstudio.models.export/src/com/netxforge/netxstudio/models/export/impl/XpandNodeToHtml.java
@@ -7,15 +7,19 @@ import org.eclipse.xpand2.XpandFacade;
 import org.eclipse.xpand2.output.Output;
 
 import com.netxforge.netxstudio.library.LibraryPackage;
+import com.netxforge.netxstudio.metrics.MetricsPackage;
 import com.netxforge.netxstudio.models.export.internal.Activator;
+import com.netxforge.netxstudio.operators.OperatorsPackage;
 
 public class XpandNodeToHtml extends XpandTemplateImpl {
 
-//	public static String XLS_OUTPUT_PATH = "export/templates";
+	// public static String XLS_OUTPUT_PATH = "export/templates";
 	public static String XPAND_NODE2HTML = "templates::node2html::Root";
-	
-	private EPackage[] metaModels = new EPackage[]{EcorePackage.eINSTANCE, LibraryPackage.eINSTANCE};
-	
+
+	private EPackage[] metaModels = new EPackage[] { EcorePackage.eINSTANCE,
+			OperatorsPackage.eINSTANCE, LibraryPackage.eINSTANCE,
+			MetricsPackage.eINSTANCE };
+
 	public String getTemplateDescription() {
 		return "Export a Network Element to HTML";
 	}
@@ -25,16 +29,18 @@ public class XpandNodeToHtml extends XpandTemplateImpl {
 	}
 
 	public XpandEmfRegistryMetaModelsImpl getEmfMetaModels() {
-		XpandEmfRegistryMetaModelsImpl reg =  XpandCallerService.addPackages(metaModels);
+		XpandEmfRegistryMetaModelsImpl reg = XpandCallerService
+				.addPackages(metaModels);
 		return reg;
 	}
 
 	@Override
 	public void xpand(IResource res) {
 		assert target != null;
-		
+
 		Output output = XpandCallerService.defineOutput(res, true);
-		XpandFacade facade = XpandCallerService.createXpandFacade(getEmfMetaModels(), getGlobalVarsMap(),  output);
+		XpandFacade facade = XpandCallerService.createXpandFacade(
+				getEmfMetaModels(), getGlobalVarsMap(), output);
 		XpandCallerService.evaluate(facade, getTemplateCall(), target);
 		Activator.logInfo(XpandCallerService.getOutput(output));
 	}
@@ -46,6 +52,5 @@ public class XpandNodeToHtml extends XpandTemplateImpl {
 	public String getExtension() {
 		return "html";
 	}
-	
-}
 
+}
