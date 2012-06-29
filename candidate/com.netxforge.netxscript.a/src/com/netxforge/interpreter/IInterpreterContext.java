0125e5c7b72553fa02caa038c9a3e4e524df038d
Christophe Bouhier <dzonekl@gmail.com>
2012-06-29 11:55:04 +0200

    not needed

--- candidate/com.netxforge.netxscript.a/src/com/netxforge/interpreter/IInterpreterContext.java
+++
@@ -1,5 +0,0 @@
-package com.netxforge.interpreter;
-
-public interface IInterpreterContext {
-
-}
--- candidate/com.netxforge.netxscript.a/src/com/netxforge/interpreter/IPrettyLog.java
+++
@@ -1,7 +0,0 @@
-package com.netxforge.interpreter;
-
-public interface IPrettyLog {
-
-	public abstract void log(String header, Object... o);
-
-}
