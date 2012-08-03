d50ccba1908a9471457162ab1e6a02024a342248
Christophe Bouhier <dzonekl@gmail.com>
2012-08-03 13:57:19 +0200

    - import helper improvement.

--- server/com.netxforge.netxstudio.server.metrics/src/com/netxforge/netxstudio/server/metrics/ServerImporterHelper.java
+++ server/com.netxforge.netxstudio.server.metrics/src/com/netxforge/netxstudio/server/metrics/ServerImporterHelper.java
@@ -18,12 +18,16 @@
  *******************************************************************************/
 package com.netxforge.netxstudio.server.metrics;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
 import org.eclipse.emf.cdo.eresource.CDOResource;
+import org.eclipse.emf.cdo.eresource.CDOResourceFolder;
+import org.eclipse.emf.cdo.eresource.CDOResourceNode;
 import org.eclipse.emf.cdo.util.CDOUtil;
+import org.eclipse.emf.cdo.util.CommitException;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -72,6 +76,8 @@ public class ServerImporterHelper implements IImporterHelper {
 	@Inject
 	private ModelUtils modelUtils;
 
+	private CDOResourceFolder netXResourceFolder = null;
+
 	public void initializeProviders(ComponentLocator networkElementLocator) {
 		// force that the same dataprovider is used
 		// so that components retrieved by the networkElementLocator
@@ -111,150 +117,220 @@ public class ServerImporterHelper implements IImporterHelper {
 			Component locatedComponent, Double dblValue, int intervalHint,
 			ComponentLocator.IdentifierDescriptor lastDescriptor) {
 
-		String path = modelUtils.cdoCalculateResourcePathII(locatedComponent);
+		if (netXResourceFolder == null) {
+			CDOResourceNode folder = importer.getDataProvider()
+					.getTransaction().getResourceNode("/Node_/");
 
-		if (path == null) {
+			// Delete any resource, which has the name "/Node_/", accidently
+			// created.
+			if (folder instanceof CDOResource) {
+				try {
+					folder.delete(null);
+					importer.getDataProvider().getTransaction().commit();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (CommitException e) {
+					e.printStackTrace();
+				}
+			} else if (folder instanceof CDOResourceFolder) {
+				// remember the folder, so we can create resources directly.
+				netXResourceFolder = (CDOResourceFolder) folder;
+			}
+		}
+
+		// String path =
+		// String cdoCalculateResourcePathII =
+		// modelUtils.cdoCalculateResourcePathII(locatedComponent);
+
+		// if (path == null) {
+		// if (DataActivator.DEBUG) {
+		// DataActivator.TRACE.trace(
+		// DataActivator.TRACE_IMPORT_HELPER_OPTION,
+		// "-- invalid CDO Resource path, should not happen.");
+		// }
+		// throw new java.lang.IllegalStateException(
+		// "Invalid CDO Resource path, should not happen.");
+		// }
+
+		// final Resource emfNetxResource = importer.getDataProvider()
+		// .getResource(path);
+
+		String cdoCalculateResourceName;
+		try {
+			cdoCalculateResourceName = modelUtils
+					.cdoCalculateResourceName(locatedComponent);
+
 			if (DataActivator.DEBUG) {
 				DataActivator.TRACE.trace(
 						DataActivator.TRACE_IMPORT_HELPER_OPTION,
-						"-- invalid CDO Resource path, should not happen.");
+						"-- looking for CDO resource with name:"
+								+ cdoCalculateResourceName);
 			}
-			throw new java.lang.IllegalStateException(
-					"Invalid CDO Resource path, should not happen.");
-		}
 
-		if (DataActivator.DEBUG) {
-			DataActivator.TRACE.trace(DataActivator.TRACE_IMPORT_HELPER_OPTION,
-					"-- looking for CDO resource path:" + path);
-		}
+			Resource emfNetxResource = null;
 
-		final Resource emfNetxResource = importer.getDataProvider()
-				.getResource(path);
-
-		// Set a prefetch policy so we can load a lot of objects in the first
-		// fetch.
-		// this should affect the Value objects being loaded. NetXResource-> MVR
-		// -> Value.
-		
-		if (emfNetxResource instanceof CDOResource) {
-			((CDOResource) emfNetxResource)
-					.cdoView()
-					.options()
-					.setRevisionPrefetchingPolicy(
-							CDOUtil.createRevisionPrefetchingPolicy(10));
-		}
-
-		final ValueDataKind valueDataKind = importer.getValueDataKind(column);
-		final Metric metric = valueDataKind.getMetricRef();
-
-		NetXResource foundNetXResource = null;
-		EList<EObject> objects = emfNetxResource.getContents();
-		for (final Object object : objects) {
-			final NetXResource netXResource = (NetXResource) object;
-
-			// Match the resource on component, metric and also the name as per
-			// last identifier value.
-			// Note, manually created resources, do not necessarly have a metric
-			// reference:
-			// see http://work.netxforge.com/issues/264
-			if (netXResource.getComponentRef() != null
-					&& netXResource.getComponentRef().cdoID()
-							.equals(locatedComponent.cdoID())
-					&& netXResource.getMetricRef() != null
-					&& netXResource.getMetricRef().cdoID() == metric.cdoID()) {
-				if (lastDescriptor != null) {
-					if (!netXResource.getShortName().equals(
-							lastDescriptor.getValue())) {
-						// Try the next one, we have a descriptor, but no
-						// matching value.
-						continue;
+			if (netXResourceFolder != null) {
+				for (CDOResourceNode n : netXResourceFolder.getNodes()) {
+					if (n.getName().equals(cdoCalculateResourceName)
+							&& n instanceof CDOResource) {
+						emfNetxResource = (Resource) n;
+						if (DataActivator.DEBUG) {
+							DataActivator.TRACE.trace(
+									DataActivator.TRACE_IMPORT_HELPER_OPTION,
+									"-- found:"
+											+ emfNetxResource.getURI()
+													.toString());
+						}
+						break;
 					}
 				}
-				foundNetXResource = netXResource;
+			}
+			if (emfNetxResource == null) {
+				emfNetxResource = netXResourceFolder
+						.addResource(cdoCalculateResourceName);
 				if (DataActivator.DEBUG) {
 					DataActivator.TRACE.trace(
 							DataActivator.TRACE_IMPORT_HELPER_OPTION,
-							"-- looking for CDO resource path:" + path);
+							"-- created resource:"
+									+ emfNetxResource.getURI().toString());
 				}
+			}
+
+			// Set a prefetch policy so we can load a lot of objects in the
+			// first
+			// fetch.
+			// this should affect the Value objects being loaded. NetXResource->
+			// MVR
+			// -> Value.
+
+			if (emfNetxResource instanceof CDOResource) {
+				((CDOResource) emfNetxResource)
+						.cdoView()
+						.options()
+						.setRevisionPrefetchingPolicy(
+								CDOUtil.createRevisionPrefetchingPolicy(24));
+			}
+
+			final ValueDataKind valueDataKind = importer
+					.getValueDataKind(column);
+			final Metric metric = valueDataKind.getMetricRef();
+
+			NetXResource foundNetXResource = null;
+			EList<EObject> objects = emfNetxResource.getContents();
+			for (final Object object : objects) {
+				final NetXResource netXResource = (NetXResource) object;
+
+				// Match the resource on component, metric and also the name as
+				// per
+				// last identifier value.
+				// Note, manually created resources, do not necessarly have a
+				// metric
+				// reference:
+				// see http://work.netxforge.com/issues/264
+				if (netXResource.getComponentRef() != null
+						&& netXResource.getComponentRef().cdoID()
+								.equals(locatedComponent.cdoID())
+						&& netXResource.getMetricRef() != null
+						&& netXResource.getMetricRef().cdoID() == metric
+								.cdoID()) {
+					if (lastDescriptor != null) {
+						if (!netXResource.getShortName().equals(
+								lastDescriptor.getValue())) {
+							// Try the next one, we have a descriptor, but no
+							// matching value.
+							continue;
+						}
+					}
+					foundNetXResource = netXResource;
+
+					if (DataActivator.DEBUG) {
+						DataActivator.TRACE.trace(
+								DataActivator.TRACE_IMPORT_HELPER_OPTION,
+								"-- matching resource: "
+										+ foundNetXResource.getShortName()
+										+ " , for component: "
+										+ locatedComponent.getName()
+										+ ", on metric: " + metric.getName());
+					}
+					break;
+				}
+			}
+			if (foundNetXResource == null) {
 
 				if (DataActivator.DEBUG) {
 					DataActivator.TRACE.trace(
 							DataActivator.TRACE_IMPORT_HELPER_OPTION,
-							"-- matching resource: "
-									+ foundNetXResource.getShortName()
-									+ " , for component: "
+							"-- No Matching resource for: "
 									+ locatedComponent.getName()
-									+ ", on metric: " + metric.getName());
+									+ ", on metric: " + metric.getName()
+									+ " creating resource:");
 				}
-				break;
+				foundNetXResource = LibraryFactory.eINSTANCE
+						.createNetXResource();
+				foundNetXResource.setComponentRef(locatedComponent);
+				foundNetXResource.setMetricRef(metric);
+
+				if (lastDescriptor != null) {
+					foundNetXResource.setShortName(lastDescriptor.getValue());
+					foundNetXResource
+							.setLongName(" Auto created resource from identifier with value "
+									+ lastDescriptor.getValue());
+					foundNetXResource.setExpressionName(importer
+							.toValidExpressionName(lastDescriptor.getValue()));
+				} else {
+					foundNetXResource.setShortName(metric.getName());
+					if (metric
+							.eIsSet(MetricsPackage.Literals.METRIC__DESCRIPTION)) {
+						foundNetXResource.setLongName(metric.getDescription());
+					} else {
+						foundNetXResource.setLongName(metric.getName());
+					}
+
+					foundNetXResource.setExpressionName(importer
+							.toValidExpressionName(metric.getName()));
+
+				}
+
+				foundNetXResource.setUnitRef(metric.getUnitRef());
+				locatedComponent.getResourceRefs().add(foundNetXResource);
+				emfNetxResource.getContents().add(foundNetXResource);
+
+				// DISABLE, DOESN'T WORK FOR AUTO-CREATED COMPONENTS>
+				// importer.addToNode(networkElement, networkElement, new
+				// ArrayList<Integer>(),
+				// foundNetXResource);
 			}
-		}
-		if (foundNetXResource == null) {
+			// objects = emfNetxResource.getContents();
+
+			final Value value = GenericsFactory.eINSTANCE.createValue();
+			value.setTimeStamp(modelUtils.toXMLDate(timeStamp));
+			value.setValue(dblValue);
 
 			if (DataActivator.DEBUG) {
 				DataActivator.TRACE.trace(
 						DataActivator.TRACE_IMPORT_HELPER_OPTION,
-						"-- No Matching resource for: "
-								+ locatedComponent.getName() + ", on metric: "
-								+ metric.getName() + " creating resource:");
-			}
-			foundNetXResource = LibraryFactory.eINSTANCE.createNetXResource();
-			foundNetXResource.setComponentRef(locatedComponent);
-			foundNetXResource.setMetricRef(metric);
-
-			if (lastDescriptor != null) {
-				foundNetXResource.setShortName(lastDescriptor.getValue());
-				foundNetXResource
-						.setLongName(" Auto created resource from identifier with value "
-								+ lastDescriptor.getValue());
-				foundNetXResource.setExpressionName(importer
-						.toValidExpressionName(lastDescriptor.getValue()));
-			} else {
-				foundNetXResource.setShortName(metric.getName());
-				if (metric.eIsSet(MetricsPackage.Literals.METRIC__DESCRIPTION)) {
-					foundNetXResource.setLongName(metric.getDescription());
-				} else {
-					foundNetXResource.setLongName(metric.getName());
-				}
-
-				foundNetXResource.setExpressionName(importer
-						.toValidExpressionName(metric.getName()));
-
+						"-- try to add value to resource : "
+								+ foundNetXResource.getShortName()
+								+ " , value =" + value.getValue()
+								+ " timestamp="
+								+ modelUtils.dateAndTime(value.getTimeStamp())
+								+ ", kind="
+								+ valueDataKind.getKindHint().getName()
+								+ " , interval =" + intervalHint);
 			}
 
-			foundNetXResource.setUnitRef(metric.getUnitRef());
-			locatedComponent.getResourceRefs().add(foundNetXResource);
-			emfNetxResource.getContents().add(foundNetXResource);
+			addToValueRange(foundNetXResource, intervalHint,
+					valueDataKind.getKindHint(),
+					Collections.singletonList(value), null, null);
+			if (DataActivator.DEBUG) {
+				DataActivator.TRACE.trace(
+						DataActivator.TRACE_IMPORT_HELPER_OPTION,
+						"-- value added ");
+			}
 
-			// DISABLE, DOESN'T WORK FOR AUTO-CREATED COMPONENTS>
-			// importer.addToNode(networkElement, networkElement, new
-			// ArrayList<Integer>(),
-			// foundNetXResource);
-		}
-		// objects = emfNetxResource.getContents();
-
-		final Value value = GenericsFactory.eINSTANCE.createValue();
-		value.setTimeStamp(modelUtils.toXMLDate(timeStamp));
-		value.setValue(dblValue);
-
-		if (DataActivator.DEBUG) {
-			DataActivator.TRACE.trace(
-					DataActivator.TRACE_IMPORT_HELPER_OPTION,
-					"-- try to add value to resource : "
-							+ foundNetXResource.getShortName() + " , value ="
-							+ value.getValue() + " timestamp="
-							+ modelUtils.dateAndTime(value.getTimeStamp())
-							+ ", kind=" + valueDataKind.getKindHint().getName()
-							+ " , interval =" + intervalHint);
-		}
-
-		addToValueRange(foundNetXResource, intervalHint,
-				valueDataKind.getKindHint(), Collections.singletonList(value),
-				null, null);
-		if (DataActivator.DEBUG) {
-			DataActivator.TRACE.trace(DataActivator.TRACE_IMPORT_HELPER_OPTION,
-					"-- value added ");
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
-
 }
