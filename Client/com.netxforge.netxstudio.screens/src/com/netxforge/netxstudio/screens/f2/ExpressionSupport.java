91606a2163716c8cd869df49442da4140a7b5359
Christophe Bouhier <dzonekl@gmail.com>
2012-09-24 11:49:38 +0200

    - Documentation

--- Client/com.netxforge.netxstudio.screens/src/com/netxforge/netxstudio/screens/f2/ExpressionSupport.java
+++ Client/com.netxforge.netxstudio.screens/src/com/netxforge/netxstudio/screens/f2/ExpressionSupport.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) Sep 24, 2012 NetXForge.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ * 
+ * Contributors: Christophe Bouhier - initial API and implementation and/or
+ * initial documentation
+ *******************************************************************************/ 
 package com.netxforge.netxstudio.screens.f2;
 
 import java.util.List;
@@ -26,6 +43,11 @@ import com.netxforge.netxstudio.library.Expression;
 import com.netxforge.netxstudio.library.NetXResource;
 import com.netxforge.netxstudio.operators.Node;
 
+
+/**
+ * Helper, which can build a context. 
+ * @author Christophe
+ */
 public class ExpressionSupport {
 
 	
@@ -101,6 +123,15 @@ public class ExpressionSupport {
 		return ImmutableList.copyOf(contextList);
 	}
 
+	
+	/**
+	 * Not used.  
+	 * @param expression
+	 * @param contextList
+	 * @param doc
+	 * @deprecated
+	 * @return
+	 */
 	public List<BaseExpressionResult> testExpression(Expression expression,
 			ImmutableList<IInterpreterContext> contextList, IXtextDocument doc) {
 
