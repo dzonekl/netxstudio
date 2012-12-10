ceccd3d6c5784bb792ad11298986551a4a1d9614
Christophe Bouhier <dzonekl@gmail.com>
2012-12-10 10:34:11 +0100

    - Standalone screen, showing the Service Summary.

---
+++ Client/com.netxforge.netxstudio.screens/src/com/netxforge/netxstudio/screens/parts/ServiceMonitoringScreenViewer.java
@@ -0,0 +1,26 @@
+package com.netxforge.netxstudio.screens.parts;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IViewPart;
+
+import com.netxforge.netxstudio.screens.editing.selector.AbstractScreenViewer;
+import com.netxforge.netxstudio.screens.editing.selector.IScreen;
+
+/**
+ * 
+ * @author Christophe
+ * 
+ */
+public class ServiceMonitoringScreenViewer extends AbstractScreenViewer {
+
+	public IScreen getScreen() {
+		return null;
+	}
+
+	public void initScreen(Composite parent) {
+	}
+
+	public void editorActivated(IViewPart activeEditor) {
+
+	}
+}
