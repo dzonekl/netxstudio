168e3ac1eb070be1b92a2b386c98fcb587abee02
Christophe Bouhier <dzonekl@gmail.com>
2012-12-10 10:35:12 +0100

    - Clean-up

--- Client/com.netxforge.netxstudio.common/src/com/netxforge/netxstudio/common/model/RFSServiceSummary.java
+++ Client/com.netxforge.netxstudio.common/src/com/netxforge/netxstudio/common/model/RFSServiceSummary.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 5 dec. 2012 NetXForge.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ * 
+ * Contributors: Christophe Bouhier - initial API and implementation and/or
+ * initial documentation
+ *******************************************************************************/ 
 package com.netxforge.netxstudio.common.model;
 
 import java.util.List;
@@ -7,6 +24,13 @@ import com.netxforge.netxstudio.operators.Node;
 import com.netxforge.netxstudio.services.RFSService;
 import com.netxforge.netxstudio.services.Service;
 
+
+/**
+ * A model object, showing the summary of the Service. 
+ * 
+ * @author Christophe Bouhier
+ *
+ */
 public class RFSServiceSummary {
 
 	// Generated values.
