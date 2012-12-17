b7ad0006298feaded52ea0a55c7aa80baab7390c
Christophe Bouhier <dzonekl@gmail.com>
2012-12-17 11:30:14 +0100

    - Refactoring, use Show-in for invoking the ChartScreen

--- Client/com.netxforge.netxstudio.screens/src/com/netxforge/netxstudio/screens/f4/ServiceMonitors.java
+++ Client/com.netxforge.netxstudio.screens/src/com/netxforge/netxstudio/screens/f4/ServiceMonitors.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 17 dec. 2012 NetXForge.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ * 
+ * Contributors: Christophe Bouhier - initial API and implementation and/or
+ * initial documentation
+ *******************************************************************************/ 
 package com.netxforge.netxstudio.screens.f4;
 
 import java.util.Date;
@@ -46,6 +63,11 @@ import com.netxforge.netxstudio.services.RFSService;
 import com.netxforge.netxstudio.services.ServiceMonitor;
 import com.netxforge.netxstudio.services.ServicesPackage;
 
+/**
+ * 
+ * @author Christophe
+ *
+ */
 public class ServiceMonitors extends AbstractScreen implements
 		IDataScreenInjection {
 
@@ -70,7 +92,6 @@ public class ServiceMonitors extends AbstractScreen implements
 		addDisposeListener(new DisposeListener() {
 			public void widgetDisposed(DisposeEvent e) {
 				toolkit.dispose();
-//				obm.dispose();
 			}
 		});
 		toolkit.adapt(this);
