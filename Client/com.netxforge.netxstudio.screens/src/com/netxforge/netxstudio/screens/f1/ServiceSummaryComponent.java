4ae5f1e9eecd02cce8da19cdb776d7d6fd318c51
Christophe Bouhier <dzonekl@gmail.com>
2013-01-18 10:14:22 +0100

    - Make the service summary a component.

--- Client/com.netxforge.netxstudio.screens/src/com/netxforge/netxstudio/screens/f1/ServiceSummaryComponent.java
+++ Client/com.netxforge.netxstudio.screens/src/com/netxforge/netxstudio/screens/f1/ServiceSummaryComponent.java
@@ -14,9 +14,12 @@
  * 
  * Contributors: Christophe Bouhier - initial API and implementation and/or
  * initial documentation
- *******************************************************************************/ 
+ *******************************************************************************/
 package com.netxforge.netxstudio.screens.f1;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.swt.SWT;
@@ -27,18 +30,25 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.forms.widgets.FormText;
 import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.progress.UIJob;
 import org.eclipse.wb.swt.SWTResourceManager;
 
 import com.google.inject.Inject;
 import com.netxforge.netxstudio.common.model.ModelUtils;
 import com.netxforge.netxstudio.common.model.RFSServiceSummary;
+import com.netxforge.netxstudio.screens.editing.selector.IScreen;
+import com.netxforge.netxstudio.screens.internal.ScreensActivator;
 import com.netxforge.netxstudio.services.RFSService;
 
 /**
  * An injectable component showing the Service Summary. Summaries are created in
  * the background.
+ * 
+ * 
+ * 
  * 
  * @author Christophe Bouhier
  * 
@@ -47,12 +57,19 @@ public class ServiceSummaryComponent {
 
 	private final FormToolkit formToolkit = new FormToolkit(
 			Display.getCurrent());
-	
-	/*
-	 * Job which creates the summary. 
+
+	/**
+	 * Job which creates the summary.
 	 */
 	private RFSServiceSummaryJob job;
-	
+
+	/**
+	 * Job which refreshs the UI for the created summary.
+	 */
+	private final RefreshSummaryJob refreshSummaryJob = new RefreshSummaryJob();
+
+	private RFSServiceSummary summary;
+
 	private ModelUtils modelUtils;
 
 	private FormText formTextLastMonitor;
@@ -74,30 +91,50 @@ public class ServiceSummaryComponent {
 
 	private Composite content;
 
+	private Composite layourComponent;
+
+	
+	/** The parent IScreen needed to layout as this component will load in background **/ 
+	private IScreen parentScreen;
+
+
 	@Inject
 	public ServiceSummaryComponent(ModelUtils modelUtils) {
 		super();
 		this.modelUtils = modelUtils;
 	}
+	
+	public  void setParentScreen(IScreen parentScreen){
+		this.parentScreen = parentScreen; 
+	}
+	
 
 	public void setShowBorder(boolean showBorder) {
 		this.showBorder = showBorder;
 	}
 
 	public void buildUI(Composite parent, Object layoutData) {
-		
-		parent.addDisposeListener(new DisposeListener(){
+
+		parent.addDisposeListener(new DisposeListener() {
 
 			public void widgetDisposed(DisposeEvent e) {
-				// Cancel running service summary job. 
-				if(job.isRunning()){
-					job.cancelMonitor();
+				// Cancel running service summary job.
+				if (job != null && job.isRunning()) {
+					job.cancel();
 				}
+				refreshSummaryJob.cancel();
 			}
 		});
 
+		layourComponent = parent.getParent(); // this is likely the component to
+												// re-layout.
+
 		content = formToolkit.createComposite(parent, showBorder ? SWT.BORDER
 				: SWT.NONE);
+		if (layoutData != null) {
+			content.setLayoutData(layoutData);
+		}
+		
 		formToolkit.paintBordersFor(content);
 		content.setLayout(new GridLayout(4, false));
 
@@ -185,61 +222,23 @@ public class ServiceSummaryComponent {
 		formToolkit.paintBordersFor(formTextGreen);
 		formTextGreen.setText("G", false, false);
 	}
-	
-	
-	
+
 	/**
 	 * Creates a summary in the background, update relevant UI bits when done.
 	 */
-	private void prepServiceSummary() {
+	private void prepServiceSummary(RFSService service) {
 
 		if (job == null) {
+
 			job = new RFSServiceSummaryJob(modelUtils);
+
 			job.addNotifier(new JobChangeAdapter() {
 				@Override
 				public void done(IJobChangeEvent event) {
 
-					final RFSServiceSummary summary = job.getSummary();
-					if (summary != null) {
-						Display.getCurrent().asyncExec(
-								new Runnable() {
-
-									public void run() {
-										formTextLastMonitor.setText(summary
-												.getPeriodFormattedString(),
-												false, false);
-
-										formTextNumberOfNodes.setText(
-												new Integer(summary
-														.getNodeCount())
-														.toString(), false,
-												false);
-										formTextNumberOfResources.setText(
-												new Integer(summary
-														.getResourcesCount())
-														.toString(), false,
-												false);
-
-										formTextRed.setText(
-												new Integer(summary
-														.getRedCountResources())
-														.toString(), false,
-												false);
-										formTextAmber.setText(
-												new Integer(
-														summary.getAmberCountResources())
-														.toString(), false,
-												false);
-										formTextGreen.setText(
-												new Integer(
-														summary.getGreenCountResources())
-														.toString(), false,
-												false);
-										content.layout();
-										// Might need to layout the parent Section. 
-									}
-								});
-					}
+					summary = job.getSummary();
+					// Schedule a refresh.
+					refreshSummaryJob.schedule(100);
 				}
 			});
 		}
@@ -248,18 +247,80 @@ public class ServiceSummaryComponent {
 			// a new job here.
 			job.cancelMonitor();
 		}
+
+		job.setRFSServiceToProcess(service);
+		job.go(); // Should spawn a job processing the xls.
+
 	}
-	
+
 	/**
-	 * Inject a {@link RFSService} into the component. 
-	 * This will first create the Service Summary Model Object.  
+	 * Inject a {@link RFSService} into the component. This will first create
+	 * the Service Summary Model Object.
 	 * 
 	 * @param service
 	 */
-	public void injectData(RFSService service){
-		prepServiceSummary();
-		job.setRFSServiceToProcess(service);
-		job.go(); // Should spawn a job processing the xls.
+	public void injectData(RFSService service) {
+		prepServiceSummary(service);
 	}
 
+	private void refreshSummaryUI() {
+
+		if (summary == null) {
+			formTextLastMonitor.setText("no monitors", false, false);
+//			content.layout();
+			layourComponent.layout();
+//			parentScreen.layout();
+			return;
+		}
+
+		formTextLastMonitor.setText(summary.getPeriodFormattedString(), false,
+				false);
+
+		formTextNumberOfNodes.setText(
+				new Integer(summary.getNodeCount()).toString(), false, false);
+		formTextNumberOfResources.setText(
+				new Integer(summary.getResourcesCount()).toString(), false,
+				false);
+
+		formTextRed.setText(
+				new Integer(summary.getRedCountResources()).toString(), false,
+				false);
+		formTextAmber.setText(
+				new Integer(summary.getAmberCountResources()).toString(),
+				false, false);
+		formTextGreen.setText(
+				new Integer(summary.getGreenCountResources()).toString(),
+				false, false);
+		content.layout();
+		layourComponent.layout();
+	}
+
+	/**
+	 * Refreshes the RFS Service Summary Section.
+	 * 
+	 * @author Christophe Bouhier
+	 */
+	class RefreshSummaryJob extends UIJob {
+
+		/**
+		 * Creates a new instance of the class.
+		 */
+		public RefreshSummaryJob() {
+			super("refresh");
+			setSystem(true);
+		}
+
+		@Override
+		public IStatus runInUIThread(IProgressMonitor monitor) {
+			if (monitor.isCanceled() || content.isDisposed())
+				return new Status(IStatus.OK, ScreensActivator.PLUGIN_ID,
+						IStatus.OK, "", null);
+
+			refreshSummaryUI();
+
+			return new Status(IStatus.OK, PlatformUI.PLUGIN_ID, IStatus.OK, "",
+					null);
+		}
+
+	}
 }
