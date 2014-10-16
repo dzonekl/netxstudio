c2acf387f1388ecb76cb8e9e8473e52b83dd80e2
Christophe Bouhier <dzonekl@gmail.com>
2014-10-16 14:07:57 +0200

    An OSS2 Demo product, which should include all OSS2 capable modules.

---
+++ products/oss2/com.netxforge.oss2.product/src/com/netxforge/oss2/product/OSS2Application.java
@@ -0,0 +1,18 @@
+package com.netxforge.oss2.product;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class OSS2Application implements IApplication {
+
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+		return null;
+	}
+
+	@Override
+	public void stop() {
+
+	}
+
+}
